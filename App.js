0ceca09ac7d90a0c9279301c5a2c9e0b4cd3cf90
Joel Kohn <joel.kohn42@gmail.com>
2019-10-11 03:54:18 +0300

    initiated ThemeProvider, flexbox adjustments

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Blink from './components/Blink';
+import { ThemeProvider, Button, Text } from 'react-native-elements';
+
+const theme = {
+  Button: {
+    icon: {
+      type: 'evilicon',
+      name: 'sc-telegram'
+    }
+  }
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Blink text='what what?' />
+      <ThemeProvider theme={theme}>
+        <View style={styles.view1}>
+          <Button title='Click me!' />
+        </View>
+
+        <View style={styles.blink}>
+          <Blink />
+        </View>
+      </ThemeProvider>
     </View>
   );
 }
@@ -13,8 +31,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  view1: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  blink: {
+    flex: 1,
     justifyContent: 'center'
   }
 });
--- components/Blink.js
+++ components/Blink.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Text, Button } from 'react-native-elements';
 
 class Blink extends Component {
-  componentDidMount() {
-    // Toggle the state every second
-    setInterval(
-      () =>
-        this.setState(previousState => ({
-          isShowingText: !previousState.isShowingText
-        })),
-      1000
-    );
-  }
-
-  state = { isShowingText: true };
-
   render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-    return <Text>{this.props.text}</Text>;
+    return (
+      <View>
+        <Button title='Now me!' />
+      </View>
+    );
   }
 }
 
