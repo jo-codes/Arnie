fc637b34016a43952cffe5f61670775d86470d65
joel kohn <joel.kohn42@gmail.com>
2019-10-13 00:58:33 +0300

    a single bodypart card on main screen

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Blink from './components/Blink';
-import { ThemeProvider, Button, Text } from 'react-native-elements';
+import {
+  ThemeProvider,
+  Tile,
+  Button,
+  Text,
+  Header
+} from 'react-native-elements';
 
 const theme = {
   Button: {
@@ -9,6 +15,10 @@ const theme = {
       type: 'evilicon',
       name: 'sc-telegram'
     }
+  },
+  Tile: {
+    height: 50,
+    width: 50
   }
 };
 
@@ -16,12 +26,16 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ThemeProvider theme={theme}>
-        <View style={styles.view1}>
-          <Button title='Click me!' />
+        <View style={styles.header}>
+          <Header
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'Arnie', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'home', color: '#fff' }}
+          />
         </View>
 
-        <View style={styles.blink}>
-          <Blink />
+        <View style={styles.bodyPartTile}>
+          <Tile featured caption='Chesticles' />
         </View>
       </ThemeProvider>
     </View>
@@ -33,12 +47,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-  view1: {
-    flex: 1,
-    justifyContent: 'center'
+  bodyPartTile: {
+    flex: 1
   },
   blink: {
+    flex: 1
+  },
+  header: {
     flex: 1,
-    justifyContent: 'center'
+    alignSelf: 'stretch'
   }
 });
